f88464cabf994b5dd9c439f5f1c66a2d5c941b5e
Oracle242 <psxnva@gmail.com>
2023-02-25 13:55:05 +0700

    V1

---
+++ HW/Array/HW_2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number [5]int
+	for i := 0; i < 5; i++ {
+		fmt.Scanln(&number[i])
+		if number[i] > 0 {
+			fmt.Println("Ваше положительое число", number[i])
+			fmt.Println("Эллемент числа", i)
+			return
+		}
+	}
+}
